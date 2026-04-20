6abd90e0fdcd5660c8bc7694d4d97847f1c9a99e
Manikandaprabhu <prabhumani7112005@gmail.com>
2026-04-21 00:27:53 +0530

    new fix

--- src/app/core/services/attendance.service.ts
+++ src/app/core/services/attendance.service.ts
@@ -52,7 +52,7 @@ export class AttendanceService {
     this.errorSignal.set(null);
 
     return new Promise((resolve, reject) => {
-      this.http.get<ApiAttendance[]>(`${this.apiUrl}/attendance`).subscribe({
+      this.http.get<ApiAttendance[]>(`${BASE}/attendance`).subscribe({
         next: (records) => {
           this.isLoadingSignal.set(false);
           this.attendanceRecordsSignal.set(records || []);
